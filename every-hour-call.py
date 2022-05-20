18f76875d7fd4b5697f9b373406e2a19c28b17f2
Muzammil Dafedar <35305645+muzammildafedar@users.noreply.github.com>
2022-05-20 10:57:39 +0530

    check every hours.

---
+++ every-hour-call.py
@@ -0,0 +1,45 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+	ec2 = boto3.resource('ec2',"us-east-1")
+	instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+	instanceDontHaveTag = []
+	for instance in instances:
+		if instance.tags == None:
+		    instanceDontHaveTag.append(instance.id)
+		else:
+		    print("There is no instance without tag")
+	if len(instanceDontHaveTag) > 0 :
+			send_email(instanceDontHaveTag)
+	
+	
+	
+		    
+def send_email(instanceId):
+    ses = boto3.client('ses')
+    body = """
+	   This is gentle reminder that following the instance doesn't have the tag. instance id's are
+    """ +str(instanceId)
+
+    ses.send_email(
+	    Source = '120cs15024@gmail.com',
+	    Destination = {
+		    'ToAddresses': [
+			    '120cs15024@gmail.com'
+		    ]
+	    },
+	    Message = {
+		    'Subject': {
+			    'Data': 'Alert',
+			    'Charset': 'UTF-8'
+		    },
+		    'Body': {
+			    'Text':{
+				    'Data': body,
+				    'Charset': 'UTF-8'
+			    }
+		    }
+	    }
+    )
+    
